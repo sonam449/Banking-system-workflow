266e7c8159443479919ca9bfbd24ab41517e8cd3
sonam449 <88935407+sonam449@users.noreply.github.com>
2025-12-01 08:09:00 +0530

    Update test_cases.sql

--- test_cases.sql
+++ test_cases.sql
@@ -1,1 +1,5 @@
-CALL transfer_money(1001, 1005, 50);
+syntex - transfor_money(from_account, to_account, amount)
+CALL transfer_money(1001, 1005, 50); -- this should pass
+
+
+CALL transfer_money(1001, 1005, 500000); --- this case should fail as 1001 account don't have this much money and at the same time max transaction limit s 50,000rs. 
