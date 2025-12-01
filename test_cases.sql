b1365d38355491ac1ebef6204b9feb5bdf974458
sonam449 <88935407+sonam449@users.noreply.github.com>
2025-12-01 08:12:38 +0530

    Update test_cases.sql

--- test_cases.sql
+++ test_cases.sql
@@ -1,5 +1,11 @@
 syntex - transfor_money(from_account, to_account, amount)
-CALL transfer_money(1001, 1005, 50); -- this should pass
+CALL transfer_money(1001, 1005, 50); -- this should pass till 3 transactions then fail as the number of transaction limit as reached.
 
 
 CALL transfer_money(1001, 1005, 500000); --- this case should fail as 1001 account don't have this much money and at the same time max transaction limit s 50,000rs. 
+
+
+CALL transfer_money(1001, 1005, 5); -- this should fail as well as min transaction amount is 50rs.
+
+
+CALL transfer_money(1001, 1005, 5000000000); -- this should fail as the amount is too big compared to 1001's account balance.
